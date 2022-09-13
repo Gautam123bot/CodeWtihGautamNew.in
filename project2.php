70f96ab2f1c93041603fc844c0dce8dbea6cd73d
Gautam <0101gautamkumar@gmail.com>
2022-09-13 15:08:59 +0530

    another owl carousel commit

---
+++ project2.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gautam Kumar | My Portfolio</title>
+    <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+    <!-- <script src="js/bootstrap.bundle.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script> -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- <script src="js/bootstrap.js"></script> -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="laptopmode.css">
+    <link rel="stylesheet" href="tabletmode.css">
+    <link rel="stylesheet" href="mobilemode.css">
+    <link rel="stylesheet" href="laptop1380pxless.css">
+    <script src="app.js"></script>
+    <link rel="icon" type="image/x-icon" href="img/profile_photo.jpeg">
+    <script src="https://kit.fontawesome.com/b90099deb6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+    <script src="jquery/jquery-3.6.1.min.js"></script>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+        integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+
+    <!-- fancybox -->
+    <link rel='stylesheet prefetch'
+        href='https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.css'>
+    <!-- magnific-popup -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css" />
+
+    <!-- Mixitup -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.2.2/mixitup.min.js'></script>
+    <!-- fancybox -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.js'>
+    </script>
+    <!-- Fancybox js -->
+
+</head>
+
+<body style="background: navajowhite; color: black;">
+    <br><br><br>
+    <div class="container-fluid main-contain p-0">
+        <div class="content">
+            <div class="container project-contain">
+                <h2 class="project-title">Project Title 2</h2>
+                <div class="row">
+                    <div class="col-lg-6 col-sm-12 col-12">
+
+                        <!-- <div class="project-div-img">
+                            <img src="img/project-1.jpg" alt="" class="img-responsive project-img">
+                            <img src="img/project-5.jpg" alt="" class="img-responsive project-img">
+                        </div> -->
+                        <div class="owl-carousel owl-theme">
+                            <div class="item mx-2">
+                                <img src="img/project-1.jpg" alt="" class="img-responsive">
+                            </div>
+                            <div class="item mx-2">
+                                <img src="img/project-5.jpg" alt="" class="img-responsive">
+                            </div>
+                        </div>
+
+                        <script>
+                        $('.owl-carousel').owlCarousel({
+                            loop: true,
+                            margin: 10,
+                            nav: true,
+                            responsive: {
+                                0: {
+                                    items: 1
+                                },
+                                600: {
+                                    items: 1
+                                },
+                                1200: {
+                                    items: 1
+                                },
+                                1604: {
+                                    items: 1
+                                }
+                            }
+                        })
+                        </script>
+
+
+                    </div>
+                    <div class="col-lg-6 col-sm-12 col-12">
+
+                        <div class="project-content">
+                            <h4>Project Info:</h4>
+                            <p class="project-info-para">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                Voluptatem nesciunt
+                                aspernatur, quas doloremque rerum minima aperiam voluptates officia dolores odit.
+                                Lorem ipsum dolor sit amet.</p>
+                            <h4 class="mt-4">Project Details:</h4>
+                            <p class="project-label"><label>Client: &nbsp;&nbsp;</label>Ruby Clinton</p>
+                            <p class="project-label"><label>Technologies: &nbsp;&nbsp;</label>iOS, HTML5, CSS3, PHP,
+                                Java</p>
+                            <p class="project-label"><label>Industry: &nbsp;&nbsp;</label>Art & Design</p>
+                            <p class="project-label"><label>Date: &nbsp;&nbsp;</label>July 16, 2019</p>
+                            <p class="project-label"><label>URL: &nbsp;&nbsp;</label><span
+                                    class="project-url">www.example.com</span></p>
+                            <p class="project-label"><label>Share: &nbsp;&nbsp;</label>
+                                <span class="project-icon">
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fa-brands fa-telegram"></i></a>
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fa-solid fa-envelope"></i></a>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="footer" style="padding-top: 0px; background: #ffd596">
+                <div class="container" style="display: block; margin: 0 auto;">
+                    <p class="copyright_para">Copyright © 2022 Gautam. All Rights Reserved.</p>
+                    <p class="term_policy">Terms & Policy &nbsp;|&nbsp; Disclaimer</p>
+                </div>
+
+            </div> -->
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
